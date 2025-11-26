32edd5ba3b7d8f6d96a37578217405f6afa16830
G4htk <gabriel290303@edu.unipar.br>
2025-11-25 21:13:13 -0300

    att3

--- src/main/java/br/unipar/devbackend/cadastroaluno2/service/DisciplinaService.java
+++ src/main/java/br/unipar/devbackend/cadastroaluno2/service/DisciplinaService.java
@@ -1,7 +1,40 @@
 package br.unipar.devbackend.cadastroaluno2.service;
 
+import br.unipar.devbackend.cadastroaluno2.model.Disciplina;
+import br.unipar.devbackend.cadastroaluno2.repository.DisciplinaRepository;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DisciplinaService {
+    @Autowired
+    private final DisciplinaRepository disciplinaRepository;
+    public DisciplinaService(DisciplinaRepository disciplinaRepository){
+        this.disciplinaRepository = disciplinaRepository;
+    }
+
+    public Disciplina salvar(Disciplina disciplina){
+        return disciplinaRepository.save(disciplina);
+    }
+
+    public List<Disciplina> listar(){
+        return disciplinaRepository.findAll();
+    }
+
+    public Disciplina atualizar(Disciplina ds, Long id){
+        return disciplinaRepository.findById(id).map(disciplina -> {
+            disciplina.setCodigo(ds.getCodigo());
+            disciplina.setDescricao(ds.getDescricao());
+            disciplina.setEmenta(ds.getEmenta());
+            disciplina.setProfessor(ds.getProfessor());
+            return disciplinaRepository.save(disciplina);
+        }).orElseThrow(()-> new RuntimeException("Disciplina não encontrada"));
+    }
+
+    public Disciplina buscarPorId(Long id){
+        return disciplinaRepository.findById(id).orElseThrow(()->new RuntimeException("Id não encontrado"));
+    }
 }
--- src/main/java/br/unipar/devbackend/cadastroaluno2/service/ProfessorService.java
+++ src/main/java/br/unipar/devbackend/cadastroaluno2/service/ProfessorService.java
@@ -1,7 +1,39 @@
 package br.unipar.devbackend.cadastroaluno2.service;
 
+import br.unipar.devbackend.cadastroaluno2.model.Professor;
+import br.unipar.devbackend.cadastroaluno2.repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class ProfessorService {
+    @Autowired
+    private final ProfessorRepository professorRepository;
+    public ProfessorService(ProfessorRepository professorRepository){
+        this.professorRepository = professorRepository;
+    }
+
+    public Professor salvar(Professor professor){
+        return professorRepository.save(professor);
+    }
+
+    public Professor buscarPorId(Long id){
+        return professorRepository.findById(id).orElseThrow(() -> new RuntimeException("id não encontrado"));
+    }
+
+    public List<Professor> buscarTodos(){
+        return professorRepository.findAll();
+    }
+
+    public Professor atualizar(Professor pf, Long id){
+        return professorRepository.findById(id).map(professor -> {
+            professor.setNome(pf.getNome());
+            professor.setCpf(pf.getCpf());
+            professor.setMatricula((pf.getMatricula()));
+            return professorRepository.save(professor);
+        }).orElseThrow(() -> new RuntimeException("Não encontrado"));
+    }
 }
